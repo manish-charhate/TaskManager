679da5a7a322329c500f84057aa44386808eb5bb
Manish Charhate <66906444+manish-charhate@users.noreply.github.com>
2024-06-21 18:27:45 +0530

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Manish Charhate on 21/06/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
