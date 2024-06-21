f3e930e2a79f4feb3e880b97be597b0a9f658748
Manish Charhate <66906444+manish-charhate@users.noreply.github.com>
2024-06-21 18:37:07 +0530

    Install and setup Firebase sdk

---
+++ TaskManager/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  TaskManager
+//
+//  Created by Manish Charhate on 21/06/24.
+//
+
+import UIKit
+import Foundation
+import FirebaseCore
+
+final class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        // Configure Firebase app
+        FirebaseApp.configure()
+        return true
+    }
+}
